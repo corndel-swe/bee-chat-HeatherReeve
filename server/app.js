299e7ebf7c9c880ffd7812e9a70e29722dd227b0
heather Reeve <heather.reeve@scope.org.uk>
2024-09-19 14:42:59 +0100

    followed along

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import { WebSocketServer } from 'ws'
+
+const server = new WebSocketServer({ port: 5001 })
+
+server.on('connection', (socket) => {
+socket.on ('message', (message) => {
+ const parsedMessage = JSON.parse(message)
+    broadcast( parsedMessage.content)
+
+})
+})
+
+function broadcast (content) {
+    server.clients.forEach(client => {
+        const sendable = JSON.stringify({
+            "content":content
+        })
+        client.send(sendable)
+    })
+}
